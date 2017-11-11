84164b4f1ba7797e72d37f297ed634c7740f8644
Felipe <fe.valdiviaa@gmail.com>
2017-11-11 14:33:20 -0600

    feature: Add new view controller to the project
    Add view controller to show the result of the game showing computer
    choice + user choice with the winner of the game.

---
+++ rock-paper-scissors/ResultViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ResultViewController.swift
+//  rock-paper-scissors
+//
+//  Created by Felipe Valdivia on 11/11/17.
+//  Copyright © 2017 Felipe Valdivia. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    var computerValue:Int?
+    var userValue:Int?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
